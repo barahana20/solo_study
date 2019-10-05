e29e7b062f7df6a8303b89c166ce14eefa6f6f19
barahana <barahana123@gmail.com>
2019-10-06 00:40:36 +0900

    정상 파일

---
+++ 지워도 됨33.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h> 
+
+int main()
+{
+	//memset(arr, 1, sizeof(arr));
+	for(int i=0 ; i<10 ; i++)
+	{
+		for(int n=0 ; n<10 ; n++)
+			printf("%d ", arr[i][n][0]);
+		printf("\n");	
+	}
+}
